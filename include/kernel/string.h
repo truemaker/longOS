c130f36649895133a3d421393b1dd72c35201d12
truemaker <thorreyer@gmail.com>
2023-03-08 12:24:03 +0100

    a little fix

--- include/kernel/string.h
+++ include/kernel/string.h
@@ -5,6 +5,6 @@ size_t strlen(const char * );
 bool strcmp(char * ,char * );
 char * strdup(const char * c);
 uint64_t oct2bin(uint8_t *str, int size);
-char* uitos(uint64_t,uint64_t,uint16_t min_digits = 0);
+char* uitos(uint64_t,uint64_t,uint16_t min_digits = 1);
 char* strcat(char*,char*);
 void strcpy(char*,char*);
